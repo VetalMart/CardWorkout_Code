cf59d465a93db6f6c9bc4e04f7ce44f13b410795
Vitalii Martynenko <7mart77@gmail.com>
2024-01-25 17:19:56 +0200

    without logic of program

---
+++ CardWorkout_Code/CWButton.swift
@@ -0,0 +1,20 @@
+//
+//  CWButton.swift
+//  CardWorkout_Code
+//
+//  Created by Віталій Мартиненко on 25.01.2024.
+//
+
+import UIKit
+
+class CWButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
